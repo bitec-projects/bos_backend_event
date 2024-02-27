2523c5d802037bc06acd4697a8dc5319596cccfc
BierOnBits <eduardo.bier@referenceit.com.br>
2024-02-27 12:17:22 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# bos_backend_event
-bos_backend_event
+## Custom Events - módulo para Bos Backend
+
+# Descrição
+
+Módulo que possibilita a criação de eventos customizados para coleções.
+
+# História de versão
+
+eduardo.bitec 27-fev-2024 - Publicando primeira versão do módulo Events Module, para uso na framework Bier On Stack (BOS).
---
+++ index.js
@@ -0,0 +1,72 @@
+var Resource = require("bos_backend/lib/resource"),
+    Script = require("bos_backend/lib/script"),
+    util = require("util");
+
+function EventResource() {
+    Resource.apply(this, arguments);
+}
+util.inherits(EventResource, Resource);
+
+EventResource.label = "Event";
+EventResource.events = ["get", "post", "put", "delete"];
+
+module.exports = EventResource;
+
+EventResource.prototype.clientGeneration = true;
+
+EventResource.prototype.handle = function (ctx, next) {
+    var parts = ctx.url.split("/").filter(function (p) {
+        return p;
+    });
+
+    var result = {};
+
+    var domain = {
+        url: ctx.url,
+        parts: parts,
+        query: ctx.query,
+        body: ctx.body,
+        this: result,
+        getHeader: function (name) {
+            if (ctx.req.headers && typeof name == "string" && name) {
+                return ctx.req.headers[name.toLowerCase()];
+            }
+        },
+        setHeader: function (name, value) {
+            if (ctx.res.setHeader) {
+                ctx.res.setHeader(name, value);
+            }
+        },
+        setStatusCode: function (statusCode) {
+            if (typeof statusCode !== "number") throw new TypeError("Status code must be a number");
+            ctx.res.statusCode = statusCode;
+        },
+        setResult: function (val) {
+            if (typeof val === "string" || typeof val === "object") {
+                result = val;
+            } else {
+                result = "" + val;
+            }
+        },
+    };
+
+    if (ctx.method === "POST" && this.events.post) {
+        this.events.post.run(ctx, domain, function (err) {
+            ctx.done(err, result);
+        });
+    } else if (ctx.method === "GET" && this.events.get) {
+        this.events.get.run(ctx, domain, function (err) {
+            ctx.done(err, result);
+        });
+    } else if (ctx.method === "DELETE" && this.events.delete) {
+        this.events.delete.run(ctx, domain, function (err) {
+            ctx.done(err, result);
+        });
+    } else if (ctx.method === "PUT" && this.events.put) {
+        this.events.put.run(ctx, domain, function (err) {
+            ctx.done(err, result);
+        });
+    } else {
+        next();
+    }
+};
